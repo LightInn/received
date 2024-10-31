91ddcbb73304fcf761a58c1db8ce62fbff06bb6c
Breval <breval2000@live.fr>
2024-10-31 03:09:44 +0100

    feat: Add character repository functions
    - Added functions for creating, retrieving, updating, and deleting characters
    - Implemented functions to fetch characters by story or all characters

---
+++ src/models/character.ts
@@ -0,0 +1,7 @@
+import {CharacterSchema} from "@/db/schema";
+
+type CharacterBase = typeof CharacterSchema.$inferSelect;
+
+export interface Character extends CharacterBase {
+    supertype?: boolean;
+}
